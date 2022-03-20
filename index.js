b015f2f71e69c4a4fb2ecd3d10e344f9ff7c4169
Orunto Eniola <oruntoeniola@aol.com>
2022-03-20 17:47:22 +0100

    ly

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-reportWebVitals();
+ReactDOM.render(<h1>Lol</h1>, document.getElementById("root"));
