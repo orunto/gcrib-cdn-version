7447539ecf68fbda77d84ae0997a2e17d74a75b4
Orunto Eniola <oruntoeniola@aol.com>
2022-03-20 18:47:15 +0100

    Reads page and some patches

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function Head () {
     return(
     <header className="App-header">
         <span id='menu'>Menu</span>
-        <img src="./logo.png" className="App-logo" />
+        <img src="../images/logo.png" className="App-logo" />
         <span id='search'>Search</span>
     </header>
     );
@@ -21,19 +21,19 @@ function Posts () {
     return (
         <section className='posts'>
         <div className='postone'>
-          <img src="./Rectangle 15.png" />
+          <img src="../images/Rectangle 15.png" />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim orci blandit at donec in nec sem tristique. Eros urna ut purus tellus pharetra arcu.</p>
           <a href='#'>Read More</a>
         </div>
 
         <div className='posttwo'>
-          <img src="./Rectangle 15.png" />
+          <img src="../images/Rectangle 15.png" />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim orci blandit at donec in nec sem tristique. Eros urna ut purus tellus pharetra arcu.</p>
           <a href='#'>Read More</a>
         </div>
 
         <div className='postthree'>
-          <img src="./Rectangle 15.png" />
+          <img src="../images/Rectangle 15.png" />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim orci blandit at donec in nec sem tristique. Eros urna ut purus tellus pharetra arcu.</p>
           <a href='#'>Read More</a>
         </div>
